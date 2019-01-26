2cdef23a6e19cec7cf8b24b7b281073bf6f108c0
sujoyDU <sujoydebnath.92@gmail.com>
2019-01-26 00:52:00 -0500

    exercise 14 is done

--- Exercise_14.py
+++ Exercise_14.py
@@ -7,7 +7,31 @@ Adventure.
 '''
 
 from sys import argv
-prog = argv
-x = input()
-print(x)
-print('{} takes input {}'.format(prog,x))
+
+# prog = argv
+# x = input()
+# print(x)
+# print('{} takes input {}'.format(prog,x))
+
+prog, user_name = argv
+prompt = '> '
+firstLine = 'Hello {}!! Welcome to my hub at {}'
+print(firstLine.format(user_name, prog))
+print('Please answer the questions below')
+print('Your Full Name: ')
+fullName = input(prompt)
+print('Where do you live?')
+location = input(prompt)
+print('Who is your spouse?')
+spouseName = input(prompt)
+print('What programming language are you using?')
+lang = input(prompt)
+
+print(
+'''Message from {}
+Hi {}. Your full name is {}. 
+You live in {}.
+Your spouse is {}. 
+You are using {} as the programming language
+'''.format(prog,user_name,fullName,location,spouseName,lang)
+)
